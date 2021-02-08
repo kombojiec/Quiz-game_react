1843e7cd00302f799d82bec1901df897f33bbb47
Kulakov Pavel <kombojiec@gmail.com>
2021-02-08 12:31:35 +0300

    add cover to side-bar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,33 +1,41 @@
-import react, {Component} from 'react';
+import  {Component} from 'react';
+import Cover from './Cover';
 const links = ['link 1', 'link 2', 'link 3', 'link 4', 'link 5']
+
 class SideBar extends Component{
   state = {
-    hidden: false,
+    hidden: true,
   }
 
   toggleSideBar = () =>{
-    this.setState({hidden: !this.state.hidden})
+    this.setState({hidden: !this.state.hidden})    
   }
 
+  closeSideBar = () => {
+    this.setState({hidden: true})
+  } 
+  
   render(){
     return(
-
-      <nav className={`side-bar ${this.state.hidden? 'side-bar_visible': 'side-bar_hidden'}`}>
-        <button type='button'
-          className={`side-bar__button `}
-          onClick={this.toggleSideBar}>
-          {this.state.hidden?<i className="fas fa-times"></i>: <i className="fas fa-bars"></i>}                
-        </button>
-        <ul className='side-bar__list'>
-          {links.map((link, index) => {
-            return(
-              <li key={index} className="side-bar__item">
-                <a href='##' className='side-bar__link'>{link}</a>
-            </li>
-            )
-          })}
-        </ul>
-      </nav>
+      <>
+        {!this.state.hidden? <Cover onClickClose={this.closeSideBar} />: null} 
+        <nav className={`side-bar ${!this.state.hidden? 'side-bar_visible': 'side-bar_hidden'}`}>
+          <button type='button'
+            className={`side-bar__button `}
+            onClick={this.toggleSideBar}>
+            {!this.state.hidden?<i className="fas fa-times"></i>: <i className="fas fa-bars"></i>}                
+          </button>
+          <ul className='side-bar__list'>
+            {links.map((link, index) => {
+              return(
+                <li key={index} className="side-bar__item">
+                  <a href='##' className='side-bar__link'>{link}</a>
+              </li>
+              )
+            })}
+          </ul>
+        </nav>
+      </>
       
     )
   }
@@ -35,8 +43,3 @@ class SideBar extends Component{
 
 export default SideBar;
 
-{/* <button type='button'
-        className={`side-bar__button ${this.state.hidden? 'side-bar_visible': 'side-bar_hidden'}`}
-        onClick={this.toggleSideBar}>
-        {this.state.hidden?<i class="fas fa-times"></i>: <i class="fas fa-bars"></i>}                
-      </button> */}
