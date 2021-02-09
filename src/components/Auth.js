20484b8bbd6841f4952000f3ae49d17ec93e3f7e
Kulakov Pavel <kombojiec@gmail.com>
2021-02-09 17:06:10 +0300

    feat(auth): create auth page

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -2,9 +2,24 @@ import React, {Component} from 'react';
 
 export default class Auth extends Component{
 
+
+  formSubmit = (event)=>{
+    event.preventDefault();
+  }
+
   render(){
     return(
-      <h1>Auth</h1>
+      <section className="section section_type_auth" >
+        <div className="auth">
+          <h1 className="auth__header"> Авторизация</h1>
+          <form onSubmit={this.formSubmit} className="auth__form">
+            <input type='text' className="auth__input" placeholder="Введите ваш em@il"/>
+            <input type='text' className="auth__input" placeholder="Введите ваш пароль" />
+            <button className="auth__button">Вход</button>
+            <button className="auth__button">Регистрация</button>
+          </form>
+        </div>
+      </section>
     )
   }
 }
